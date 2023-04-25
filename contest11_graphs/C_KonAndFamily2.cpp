48fcf2e34268146adfaacaca7f8a89d1ad8fc812
dashbah <dsbakhankova@edu.hse.ru>
2023-04-26 00:53:44 +0400

    C OK

--- contest11_graphs/C_KonAndFamily2.cpp
+++ contest11_graphs/C_KonAndFamily2.cpp
@@ -12,17 +12,42 @@ struct Guest {
     bool used = false;
 
     bool operator<(const Guest &other) const {
-        return (dist < other.dist) || (dist == other.dist && name < other.name);
+        return name < other.name;
+    }
+
+    bool operator==(const Guest &other) const {
+        return name == other.name;
     }
 };
 
-void dfs(int v, std::vector<Guest> &guests, const std::vector<std::vector<Guest>> &g) {
+std::vector<Guest> guests;
+
+int comp(const void *left, const void *right) {
+    int a = *reinterpret_cast<const int *>(left);
+    int b = *reinterpret_cast<const int *>(right);
+    if (guests[a] < guests[b]) {
+        return -1;
+    }
+    if (guests[a] == guests[b]) {
+        return 0;
+    }
+    return 1;
+}
+
+void sortLine(std::vector<int> &vector, const std::vector<Guest> &guests1) {
+    for (size_t current = 1; current < vector.size(); ++current) {
+        for (auto j = current; j > 0 && guests1[vector[j]] < guests1[vector[j - 1]]; --j) {
+            std::swap(vector[j], vector[j - 1]);
+        }
+    }
+}
+
+void dfs(int v, const std::vector<std::vector<int>> &g) {
     guests[v].used = true;
     std::cout << guests[v].name << "\n";
     for (auto &i : g[v]) {
-        if (!guests[i.id].used) {
-            // guests[i.id].dist = guests[v].dist + 1;
-            dfs(i.id, guests, g);
+        if (!guests[i].used) {
+            dfs(i, g);
         }
     }
 }
@@ -34,28 +59,31 @@ int main() {
 
     int m, n;
     std::cin >> n >> m;
-    std::vector<Guest> guests(n);
+    guests = std::vector<Guest>(n);
     for (int i = 0; i < n; ++i) {
         std::cin >> guests[i].id;
         getline(std::cin, guests[i].name);
     }
-    std::vector<std::vector<Guest>> g(m);
+    std::vector<std::vector<int>> g(n);
     int first, last;
     for (int i = 0; i < m; ++i) {
         std::cin >> first;
         std::cin >> last;
-        g[first].emplace_back(guests[last]);
-        g[last].emplace_back(guests[first]);
+        g[first].emplace_back(last);
+        g[last].emplace_back(first);
     }
 
-    for (int i = 0; i < m; ++i) {
-        std::sort(g[i].begin(), g[i].end());
+    for (int i = 0; i < n; ++i) {
+        // sortLine(g[i], guests);
+        if (!g[i].empty()) {
+            qsort(&g[i][0], g[i].size(), sizeof(int), comp);
+        }
     }
 
     int s;
     std::cin >> s;
 
-    dfs(s, guests, g);
+    dfs(s, g);
 
     return 0;
 }
